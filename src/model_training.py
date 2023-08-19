8c3e526f7af62dcf78d55657c2a3209aa5273f8e
Bereketo <bereketalebachew87@gmail.com>
2023-08-19 08:40:59 +0300

    model training

--- src/model_training.py
+++ src/model_training.py
@@ -3,8 +3,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-import joblib  # Used for saving and loading models
+import joblib
 
+label_encoder = LabelEncoder()
 def load_data(filename):
     """Load data from CSV file."""
     return pd.read_csv(filename)
@@ -32,40 +33,17 @@ def save_model(model, filename):
     """Save the trained model to disk."""
     joblib.dump(model, filename)
 
-def load_model(filename):
-    """Load a pre-trained model from disk."""
-    return joblib.load(filename)
-
-def predict_samples(model, sample_data, label_encoder):
-    """Make predictions using the pre-trained model."""
-    y_pred_onehot = model.predict(sample_data)
-    y_pred_encoded = y_pred_onehot.argmax(axis=1)
-    y_pred = label_encoder.inverse_transform(y_pred_encoded)
-    return y_pred
-
-def evaluate_model(model, X_test, y_test):
-    """Evaluate model accuracy."""
-    y_pred_encoded = model.predict(X_test)
-    accuracy = accuracy_score(y_test.argmax(axis=1), y_pred_encoded.argmax(axis=1))
-    return accuracy
 
 def main():
-    data = load_data('Crop_recommendation.csv')
+    data = load_data('../data/crop_data.csv')
 
     X, y_onehot, label_encoder = preprocess_data(data)
-
+    label_encoder = label_encoder
     X_train, X_test, y_train, y_test = train_test_split(X, y_onehot, test_size=0.2, random_state=42)
 
     rf_model = train_model(X_train, y_train)
 
     save_model(rf_model, '../models/crop_rf_model.pkl')
-
-    loaded_model = load_model('../models/crop_rf_model.pkl')
-
-    sample_data = [[82, 36, 41, 23.325013100000003, 79.79609448, 6.581693772, 187.3096148]]
-    y_pred = predict_samples(loaded_model, sample_data, label_encoder)
-
-    print(f'Predicted Crop: {y_pred[0]}')
 
 if __name__ == "__main__":
     main()
